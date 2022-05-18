ecfd5c16ec0628ebe812915ec62c9220b19145c6
Md Tausif <official.tausif.786@gmail.com>
2022-05-18 21:21:21 +0530

    serialisers customised as per need

--- user_authAPI/serializers.py
+++ user_authAPI/serializers.py
@@ -1,3 +1,5 @@
+from nbformat import read
+from numpy import source
 from rest_framework import serializers
 from django.contrib.auth.models import User
 from .models import Message
@@ -6,10 +8,12 @@ class userSerializers(serializers.ModelSerializer):
 
 	class Meta:
 		model = User
-		fields = '__all__'
+		fields = ['id','username','first_name','last_name','email']
 
 class MessageSerializers(serializers.ModelSerializer):
+    created_by = userSerializers(read_only=True)
     class Meta:
         model = Message
-        fields = '__all__'
+        fields = ['id','message','created_at','updated_at','created_by']
+
 
