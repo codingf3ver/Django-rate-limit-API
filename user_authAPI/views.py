713433b618372d714be9955befa90a603db6f2e2
Md Tausif <official.tausif.786@gmail.com>
2022-05-18 21:19:58 +0530

    Error fixed for user table record fetch

--- user_authAPI/views.py
+++ user_authAPI/views.py
@@ -1,3 +1,4 @@
+from email import message
 import json
 from textwrap import indent
 from django.shortcuts import render
@@ -12,68 +13,75 @@ from .models import Message
 from django.contrib.auth.models import User
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
-from rest_framework.authtoken.models import Token
-from rest_framework.authtoken.views import ObtainAuthToken
+
+from django.http import Http404
 # Create your views here.
 
 def home(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    return HttpResponse("Hello, world. You're at the API tesing home")
 
 class userviewsets(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = userSerializers
 
 class MessageView(APIView):
+
     authentication_classes = (TokenAuthentication,)
     permission_classes = (IsAuthenticated,)
     
-
     def get(self, request,*args, **kwargs):
-        all_messages = Message.objects.all()
-        message_serializer = MessageSerializers(data = all_messages, many=True)
-        message_serializer.is_valid()
-        current_user = request.user
-       
-        data = {
-            'user_id': current_user.id,
-            'messages': message_serializer.data,
-            'first_name': current_user.first_name,
-            'last_name': current_user.last_name,
-            'email': current_user.email,
-            }
-        json_data = json.dumps(data,indent=4)
-        with open('results.json', 'w') as f:
-            f.write(json_data)
-        return Response(data , status=status.HTTP_200_OK)
-       
+        try:
+            message = Message.objects.all()
+            message_serializer = MessageSerializers(message, many=True)
+
+            json_data = json.dumps(message_serializer.data,indent=4)
+            with open('results.json', 'w') as f:
+                f.write(json_data)
+            return Response(message_serializer.data, status=status.HTTP_200_OK)
+        except Message.DoesNotExist:
+            raise Http404
+        
     def post(self, request, *args, **kwargs):
+
         message_data = request.data
-        create_message = Message.objects.create(message=message_data['message'])
+        current_user = request.user
+
+        create_message = Message.objects.create(message=message_data['message'],created_by_id=current_user.id)
         create_message.save()
-        # serializer = MessageSerializers(create_message)
+        
         data={ 
             'message': 'success',
             'status': status.HTTP_201_CREATED
         }
+        with open('messages.json', 'w') as outfile:
+            json.dump(data, outfile)
 
         return Response(data ,status=status.HTTP_201_CREATED)
     
     def put(self, request, *args, **kwargs):
         message_data = request.data
-        id = message_data['id']
-        Message.objects.filter(id=id).update(message=message_data['message'],updated_at=message_data['updated_at'])
-        data={ 
-            'message': 'message updated',
-            'status': status.HTTP_201_CREATED
-        }
-        return Response(data ,status=status.HTTP_201_CREATED)
+        try:
+            message_id = message_data['id']
+            Message.objects.filter(id=message_id).update(message=message_data['message'],updated_at=message_data['updated_at'])
+            data={ 
+                'message': 'message updated',
+                'status': status.HTTP_201_CREATED
+            }
+            return Response(data ,status=status.HTTP_201_CREATED)
+        except Message.DoesNotExist:
+            raise Http404
 
     def delete(self, request, *args, **kwargs):
         message_data = request.data
-        message_id = message_data['id']
-        Message.objects.filter(id=message_id).delete()
-        data={ 
-            'message': 'user deleted',
-            'status': status.HTTP_201_CREATED
-        }
-        return Response(data ,status=status.HTTP_201_CREATED)
+
+        try:
+            message_id = message_data['id']
+            Message.objects.filter(id=message_id).delete()
+            data={ 
+                'message': 'user deleted',
+                'status': status.HTTP_201_CREATED
+            }
+            return Response(data ,status=status.HTTP_201_CREATED)
+
+        except Message.DoesNotExist:
+            raise Http404
