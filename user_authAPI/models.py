13f60db6af5dbfe512c5f82f3b9a850026d36344
Md Tausif <official.tausif.786@gmail.com>
2022-05-18 21:22:29 +0530

    model updated as per the requirements

--- user_authAPI/models.py
+++ user_authAPI/models.py
@@ -1,6 +1,5 @@
 from django.db import models
-
-# Create your models here.
+from django.contrib.auth.models import User
 
 
 class Message(models.Model):
@@ -8,6 +7,8 @@ class Message(models.Model):
     message = models.CharField(max_length=500)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    created_by = models.ForeignKey(User, on_delete=models.CASCADE, related_name='created_by')
+
     
     def __str__(self):
         return self.message
